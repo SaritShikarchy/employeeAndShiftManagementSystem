9d738cb8b32985f72a87c645ac546103a52d1d07
Sarit Shikarchy <saritshik@gmail.com>
2025-09-08 16:03:28 +0300

    removing comments

--- server/routers/usersActionsRouter.js
+++ server/routers/usersActionsRouter.js
@@ -10,29 +10,18 @@ try{
    
     const date = todayStr(); 
     //updateActionAllowd checks if the record of  user_id+date exist
-    // if the record exist then it increase the number of 'actionAllowd'
+    //if the record exist then it increase the number of 'actionAllowd'
     //if the record doesn't exist then err is thrown ('DAILY_LIMIT'/ 'NOT FOUND') 
     const rec=await updateActionAllowd (userId, date)
-    //return res.json ({ok:true, record: rec})
     return res.status(200).json({ ok: true, record: rec });
     }
 
-//the below errors come from performing {updateActionAllowd}
-// catch(e){
-//      console.error(e); 
-//     if (e.code === 'DAILY_LIMIT') return res.status(403).json ({error: e.message });
-//     if (e.code === 'NOT FOUND')   return res.status(404).json ({error: e.message });
-//     return res.status(500).json({ error: 'server error' , msg:'server'});
-// }
-
 catch (e) {
-  console.error('usersActionsRouter error:', e);
+  console.error(e);
   if (e.code === 'DAILY_LIMIT') return res.status(403).json({ error: e.message });
   if (e.code === 'NOT FOUND')   return res.status(404).json({ error: e.message });
   return res.status(500).json({ error: 'server error', detail: e.message });
 }
-
-
 })
 
 module.exports =router;
